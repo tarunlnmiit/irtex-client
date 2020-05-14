abe1f990e81169753cae36f53a745e282507a245
Tarun Gupta <tarungupta.y12@lnmiit.ac.in>
2020-05-14 20:23:57 +0200

    heroku setup

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 app.use(express.static(__dirname + '/dist'));
 
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 4200);
 
 // Path Location Starategy
 app.get('/*', (req, res) => {
