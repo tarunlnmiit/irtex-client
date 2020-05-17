3b8cf4a62d64fec74c2df067ceec8add3e128094
ritugahir <ritugahir4@gmail.com>
2020-05-18 04:16:21 +0530

    app component first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { AppService } from './app.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'irtex-client';
-  userdata;
-  constructor(private appService: AppService) {}
-  ngOnInit() {
-    this.userdata = this.appService.getUserData();
-  }
+  title = 'frontend';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,50 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { from } from 'rxjs';
 
 import { AppComponent } from './app.component';
+import { SearchFormComponent } from './component/search-form/search-form.component';
+import { SearchResultsComponent } from './component/search-results/search-results.component';
+import { HomePageComponent } from './component/home-page/home-page.component';
 
-import { HttpClientModule } from '@angular/common/http';
-import { RouterModule } from '@angular/router';
-import { AppRoutingModule } from './app-routing.module';
+import { SearchService } from './services/search.service';
+
+const routes: Routes = [
+  { path: 'results', component: SearchResultsComponent },
+  { path: '', component: HomePageComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchFormComponent,
+    SearchResultsComponent,
+    HomePageComponent,
   ],
   imports: [
+    MatTabsModule,
+    MatPaginatorModule,
+    BrowserAnimationsModule,
+    MatCheckboxModule,
+    MatFormFieldModule,
+    MatSelectModule,
     BrowserModule,
     HttpClientModule,
-    RouterModule,
-    AppRoutingModule
+    ReactiveFormsModule,
+    MatProgressSpinnerModule,
+    RouterModule.forRoot(routes, { onSameUrlNavigation: 'reload' }),
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [SearchService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
