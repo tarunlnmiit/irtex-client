14b4ffdc55b4bd6b9589ea92394f0749993ac222
Tarun Gupta <tarungupta.y12@lnmiit.ac.in>
2020-07-02 22:06:02 +0200

    ngrok test

--- src/app/common/settings.ts
+++ src/app/common/settings.ts
@@ -1,3 +1,4 @@
 export class Settings {
-  public static baseUrl = 'http://localhost:8000';
+  // public static baseUrl = 'http://localhost:8000';
+  public static baseUrl = 'https://80aa1369a498.ngrok.io';
 }
